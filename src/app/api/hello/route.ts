c1244592348d3bd781c68e5f4e31f8458443c64e
SwallowAI <alex@swallow.ai>
2025-05-22 14:05:56 +0000

    feat: migrate apis

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,13 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from 'next/server';
 
 type Data = {
   name: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+export function GET(request: Request) {
+  const data: Data = { name: "John Doe" };
+  return NextResponse.json(data, { status: 200 });
 }
